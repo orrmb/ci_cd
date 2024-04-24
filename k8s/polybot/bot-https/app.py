06def5bf366b2db2bb217a314eccd7a45dc192e2
orrmb <orr4353@gmail.com>
2024-04-24 14:18:12 +0300

    update project

--- k8s/polybot/bot-https/app.py
+++ k8s/polybot/bot-https/app.py
@@ -13,7 +13,7 @@ import signal
 
 app = flask.Flask(__name__)
 
-TELEGRAM_APP_URL = 'https://orb-polybot-dev.devops-int-college.com:8443'
+TELEGRAM_APP_URL = 'https://orb-polybot-dev.devops-int-college.com:8444'
 TABLE_NAME = os.environ['TABLE_NAME']
 
 WEBHOOK_SSL_CERT = './certificate/YOURPUBLIC-dev.pem'
