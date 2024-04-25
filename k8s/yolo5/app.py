1674f632f80e41a74b65fad66a3858ef8bf14fe8
orrmb <orr4353@gmail.com>
2024-04-25 11:40:47 +0300

    yolo-dev version 0.0.8

--- k8s/yolo5/app.py
+++ k8s/yolo5/app.py
@@ -103,5 +103,6 @@ def consume():
             # Delete the message from the queue as the job is considered as DONE
             sqs_client.delete_message(QueueUrl=queue_name, ReceiptHandle=receipt_handle)
 
+
 if __name__ == "__main__":
     consume()
